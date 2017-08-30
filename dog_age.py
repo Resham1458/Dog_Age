022bf16b64d1d850a49e6c762d92b63068988d9f
Resham1458 <anuresham2015@gmail.com>
2017-08-30 14:11:57 -0400

    updated

--- dog_age.py
+++ dog_age.py
@@ -1,11 +1,11 @@
 def gogo():
     age = int(input("Enter your dog's age in human years:"))
-    if age < 0:
+    if age < 0: #checks whether the input number is positive or negative
         print("Age must be positive number.")
         print("Try again")
         gogo()
 
     else:
-        dog_years = age / 7
-        print("Your dog's age in dog's years is", dog_years)
+        dog_years = age / 7 #converts the dog’s age in human years into dog’s years
+        print("Your dog's age in dog's years is", "{0:.2f}".format(dog_years))
 gogo()
